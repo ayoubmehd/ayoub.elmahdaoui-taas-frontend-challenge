cc1524d5dcd1fff0dc8b293326165a5af1662d1c
ayoubmehd <safi-_-gio-raja@live.fr>
2022-03-10 20:01:45 +0100

    feat(repository page): build sidebar and navitem

---
+++ src/components/Sidebar/NavItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="flex items-center py-3 px-5">
+    <router-link class="text-indigo-400 inline-flex items-center" to="/">
+      <slot></slot>
+    </router-link>
+  </li>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,15 @@
+<template>
+  <aside
+    class="bg-gray-100 shadow-sm rounded sticky top-0 md:w-1/6 mb-auto mx-2 overflow-auto"
+  >
+    <ul class="flex md:flex-col justify-center py-3">
+      <slot></slot>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
