62b3da3354d145368a76f7ac75e6b0f3c50491ad
ayoubmehd <safi-_-gio-raja@live.fr>
2022-03-16 10:39:30 +0100

    refactor: clean up app component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <Alert>
-    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, id!
-  </Alert>
+  <Alert> </Alert>
   <div class="container mx-auto px-4 md:flex pt-12 justify-center">
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
+import { useStore } from "vuex";
 import { Options, Vue } from "vue-class-component";
-import Alert from "@/components/Alert.vue";
+import Alert from "./components/Alert.vue";
+import { ref } from "@vue/reactivity";
 
 @Options({
   components: {
