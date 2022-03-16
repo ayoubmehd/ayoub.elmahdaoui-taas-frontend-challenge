1ed84929b7d2db70f01c22556ed120e2905d6519
ayoubmehd <safi-_-gio-raja@live.fr>
2022-03-16 10:36:49 +0100

    feat: show alert using global state

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,13 +1,32 @@
 import { createStore } from "vuex";
 import github from "@/store/github.store";
 
-export default createStore({
-  mutations: {},
-  actions: {},
-  modules: {
-    github: {
-      namespaced: true,
-      ...github,
+export default createStore<{ alertMessage: string; showAlert: boolean }>({
+  state: {
+    alertMessage: "",
+    showAlert: false,
+  },
+  mutations: {
+    setAlertMessage(state, payload) {
+      state.alertMessage = payload;
     },
+    setShowAlert(state, payload) {
+      state.showAlert = payload;
+    },
+  },
+  actions: {
+    showAlert({ commit, dispatch }, { message, amount }) {
+      commit("setAlertMessage", message);
+      commit("setShowAlert", true);
+      setTimeout(() => {
+        dispatch("hideMessage");
+      }, amount || 5000);
+    },
+    hideMessage({ commit }) {
+      commit("setShowAlert", false);
+    },
+  },
+  modules: {
+    github,
   },
 });
