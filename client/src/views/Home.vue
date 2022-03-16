9bb9d81a3f59cbe919739a70449241ad054634ba
ayoubmehd <safi-_-gio-raja@live.fr>
2022-03-16 06:46:13 +0100

    feat: add auth link to the github auth link

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,16 +1,32 @@
 <template>
-  <button
+  <a
+    :href="url"
     class="flex-shrink-0 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg mt-10 sm:mt-0"
   >
     Authorize Access to Github
-  </button>
+  </a>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
+import { test } from "../api/index";
+import { oauthAuthorizationUrl } from "@octokit/oauth-authorization-url";
 
 @Options({
   components: {},
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  url = "";
+
+  created() {
+    const { url, clientId, redirectUrl, login, state } = oauthAuthorizationUrl({
+      clientType: "oauth-app",
+      clientId: "c1bdaf5d98a6e734df13",
+      redirectUrl: "http://localhost:8080/continue",
+      login: "octocat",
+      scopes: ["repo"],
+    });
+    this.url = url;
+  }
+}
 </script>
